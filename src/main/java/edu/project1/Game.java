5697a1bdf13b7cf71e4e38c86aea5c23eb29c699
Ivan Gr <drivergood.warchief@gmail.com>
2023-10-23 03:56:32 +0700

    исправленный стиль

--- src/main/java/edu/project1/Game.java
+++ src/main/java/edu/project1/Game.java
@@ -5,59 +5,60 @@ import java.util.List;
 
 public class Game {
     private boolean endofGame = false;
-    private int maxMistakes=6;
-    private int currentMistake=0;
+    private int maxMistakes = 6;
+    private int currentMistake = 0;
     private static String endWorld = "EXIT";
 
-    List<Character> guesedCorrectLatters=new ArrayList<>();
-    List<Character> guesedMistakeLatters=new ArrayList<>();
-    HiddenWord hiddenWord= new HiddenWord();
+    List<Character> guesedCorrectLatters = new ArrayList<>();
+    List<Character> guesedMistakeLatters = new ArrayList<>();
+    HiddenWord hiddenWord = new HiddenWord();
 
-    private boolean checkrepeatLater(String input){
-        if (input.length()>1){
+    private boolean checkrepeatLater(String input) {
+        if (input.length() > 1) {
             return true;
-        } else if (guesedMistakeLatters.indexOf(input)>=0) {
+        } else if (guesedMistakeLatters.indexOf(input) >= 0) {
             return true;
-        }else if (guesedMistakeLatters.indexOf(input)>=0){
+        } else if (guesedMistakeLatters.indexOf(input) >= 0) {
             return true;
-        }else {
+        } else {
             return false;
         }
     }
-    private boolean lose(){
-        return currentMistake>maxMistakes;
+
+    private boolean lose() {
+        return currentMistake > maxMistakes;
     }
 
-    public boolean Gamerun(){
+    public boolean gamerun() {
         hiddenWord.getRandomWorld();
         hiddenWord.hideWord();
-        System.out.println("Добро пожаловать.  Для выхода используется EXIT" );
-        while (!endofGame){
+        System.out.println("Добро пожаловать.  Для выхода используется EXIT");
+        while (!endofGame) {
             hiddenWord.printHiddenWorld();
             System.out.println("Введите букву");
             String input = hiddenWord.getlatter();
-            while (checkrepeatLater(input)){
-                if (input=="EXIT"){
-                    endofGame=true;
+            while (checkrepeatLater(input)) {
+                if (input.equalsIgnoreCase(endWorld)) {
+                    endofGame = true;
                     break;
                 }
-                input= hiddenWord.getlatter();
+                input = hiddenWord.getlatter();
             }
-            if(hiddenWord.checklater(input.toCharArray()[0])){
+            if (hiddenWord.checklater(input.toCharArray()[0])) {
                 guesedCorrectLatters.add(input.toCharArray()[0]);
                 hiddenWord.openlatter(input.toCharArray()[0]);
-            }else {
+            } else {
                 guesedMistakeLatters.add(input.toCharArray()[0]);
                 currentMistake++;
-                System.out.println("Количество ошибок "+currentMistake+" Из "+ maxMistakes);
+                System.out.println("Количество ошибок " + currentMistake + " Из " + maxMistakes);
             }
-            if(lose()){
-                endofGame=true;
+            if (lose()) {
+                endofGame = true;
                 System.out.println("You lose");
                 return false;
             }
-            if(hiddenWord.victory()){
-                endofGame=true;
+            if (hiddenWord.victory()) {
+                endofGame = true;
                 System.out.println("Congratulations");
                 return true;
             }
--- src/main/java/edu/project1/HiddenWord.java
+++ src/main/java/edu/project1/HiddenWord.java
@@ -1,66 +1,64 @@
 package edu.project1;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
-
 public class HiddenWord {
-    private static String[] dictionary = new String[]{
-        "hello","world","commit",
-        "push","class","interface",
-        "array","integer","good","evening"
-    }
-        ;
-    private String HidenWord;
-    private String WordInStars;
+    private static String[] dictionary = new String[] {
+        "hello", "world", "commit",
+        "push", "class", "interface",
+        "array", "integer", "good", "evening"
+    };
+    private String hidenWord;
+    private String wordInStars;
     Random randomWord = new Random();
     Scanner scannerForLetter = new Scanner(System.in);
 
-    public HiddenWord(){
-        this.HidenWord="";
-        this.WordInStars="";
+    public HiddenWord() {
+        this.hidenWord = "";
+        this.wordInStars = "";
     }
 
-    public void openlatter(char letter){
-        for (int i=0;i < HidenWord.length();i++){
-            if(HidenWord.toCharArray()[i]==letter){
-                WordInStars = WordInStars.substring(0, i) + letter + WordInStars.substring( i + 1);
+    public void openlatter(char letter) {
+        for (int i = 0; i < hidenWord.length(); i++) {
+            if (hidenWord.toCharArray()[i] == letter) {
+                wordInStars = wordInStars.substring(0, i) + letter + wordInStars.substring(i + 1);
             }
         }
     }
-    public void hideWord(){
-        for (int i=0;i < HidenWord.length();i++){
-            WordInStars+="*";
+
+    public void hideWord() {
+        for (int i = 0; i < hidenWord.length(); i++) {
+            wordInStars += "*";
         }
     }
 
-    public void getRandomWorld(){
-        int randomnumber= randomWord.nextInt(dictionary.length);
-        this.HidenWord=dictionary[randomnumber];
+    public void getRandomWorld() {
+        int randomnumber = randomWord.nextInt(dictionary.length);
+        this.hidenWord = dictionary[randomnumber];
     }
 
-    public String getlatter(){
+    public String getlatter() {
         return scannerForLetter.next();
     }
-    public boolean checklater(char letter){
-        if(HidenWord.indexOf(letter)>=0){
+
+    public boolean checklater(char letter) {
+        if (hidenWord.indexOf(letter) >= 0) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
-    public void printHiddenWorld(){
-        System.out.println(WordInStars);
+
+    public void printHiddenWorld() {
+        System.out.println(wordInStars);
     }
-    public boolean victory(){
-        if (WordInStars.indexOf('*')==-1){
+
+    public boolean victory() {
+        if (wordInStars.indexOf('*') == -1) {
             return true;
         }
         return false;
     }
-
 
 }
