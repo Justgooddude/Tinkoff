5bed0e944ce45bafdee8e666264c02a231c6e46d
Ivan Gr <drivergood.warchief@gmail.com>
2023-12-01 20:19:17 +0700

    made testing method

---
+++ src/main/java/edu/project1/Main.java
@@ -0,0 +1,12 @@
+package edu.project1;
+
+public class Main {
+    private Main() {
+
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        Boolean result = game.gamerun();
+    }
+}
